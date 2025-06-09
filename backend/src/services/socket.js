6689c95843250b4a3b02f1fe66e7a990ecd592b8
Sumit Kesarwani <agentsumit007@gmail.com>
2025-06-09 16:46:19 +0530

    cors error fixed

--- backend/src/services/socket.js
+++ backend/src/services/socket.js
@@ -18,13 +18,13 @@ const corsConfig = {
 const app = express();
 const server = http.createServer(app);
 
-// const io = new Server(server);
-// app.use(cors());
+const io = new Server(server);
+app.use(cors());
 
-const io = new Server(server, {
-  cors: corsConfig,
-});
-app.use(cors(corsConfig));
+// const io = new Server(server, {
+//   cors: corsConfig,
+// });
+// app.use(cors(corsConfig));
 
 app.use(express.json());
 
