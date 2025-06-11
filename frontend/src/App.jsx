6eaee72b2fe5b2703ada2edad2662010887f4bf3
Sumit Kesarwani <agentsumit007@gmail.com>
2025-06-11 13:06:25 +0530

    credentials added

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -14,7 +14,7 @@ const App = () => {
       <Route
         path="/"
         element={
-          <MainLayout>
+          <MainLayout hasFooter>
             <Home />
           </MainLayout>
         }
--- frontend/src/Components/layout/main-layout/footer.jsx
+++ frontend/src/Components/layout/main-layout/footer.jsx
@@ -1,7 +1,24 @@
+import { Github, Linkedin } from "lucide-react";
 import React from "react";
+import { config } from "../../../../config";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="h-[50px] bg-base-300 flex justify-between items-center py-10 px-15">
+      <h1 className="text-md font-bold">made by Sumit Kesarwani</h1>
+      <div className="flex justify-end gap-8">
+        <a href="https://github.com/agentsumit007" target="_blank">
+          <Github size={40} />
+        </a>
+        <a
+          href="https://www.linkedin.com/in/sumit-kesarwani-a62a21194/"
+          target="_blank"
+        >
+          <Linkedin size={40} />
+        </a>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
